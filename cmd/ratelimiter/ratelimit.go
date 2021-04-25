a270ef18a12f840bfa82e0c5c1840e9a9372dbe2
parth105 <parthrshah@live.com>
2021-04-25 12:15:59 -0700

    Restructuring the code;
    a. Changed the nested DS for userDB to a list, containing
    two ints, current time interval and number of requests.
    This allows simpler time interval calculation.
    
    b. Better commenting for debugging.

--- cmd/ratelimiter/ratelimit.go
+++ cmd/ratelimiter/ratelimit.go
@@ -1,47 +1,49 @@
 package ratelimiter
 
 import (
+	"fmt"
 	"time"
 )
 
 type RateLimit struct {
-	userDB       map[string]map[int64]int64
+	userDB       map[string][]int64
 	window       int64
 	requestLimit int64
 	//backendUrl		string
 }
 
-func NewRateLimit(w int64, r int64) *RateLimit {
+func NewRateLimit(w, r int64) *RateLimit {
 	return &RateLimit{
-		userDB:       map[string]map[int64]int64{},
+		userDB:       map[string][]int64{},
 		window:       w,
 		requestLimit: r,
 	}
 }
 
-func (r *RateLimit) Call(user string) bool {
+func (r *RateLimit) Call(user string) (rateLimited bool) {
 	now := time.Now()
 	timeInt := now.Unix()
 
-	// Return true if the user is within the time window or the number of requests
-	if _, i := r.userDB[user]; !i {
-		r.userDB[user] = map[int64]int64{}
-		r.userDB[user][timeInt] = 1
-		//fmt.Printf("Call #1, for new user %s in time interval %v, logged with # of requests 1; returning ", user, timeInt)
-		return true
-	} else {
-		if _, i := r.userDB[user][timeInt]; !i {
-			r.userDB[user][timeInt] = 1
-			//fmt.Printf("Call #1, for existing user %s in time interval %v, logged with # of requests 1; returning ", user, timeInt)
-			return true
-		} else {
-			r.userDB[user][timeInt] += 1
-			//fmt.Printf("Call #%v, for existing user %s in time interval %v; returning ", r.userDB[user][timeInt], user, timeInt)
-			if r.userDB[user][timeInt] > r.requestLimit {
-				return false
-			} else {
-				return true
+	// Returns val, true if the user is within the time window or the number of requests
+	if _, i := r.userDB[user]; !i { // New user, add to DB
+		r.userDB[user] = []int64{timeInt, 1}
+		fmt.Printf("Call #1, for new user %s in time interval %v, logged with # of requests 1; ratelimited: ", user, timeInt)
+		rateLimited = false
+	} else { // Existing user, check timeinterval
+		if s := r.userDB[user]; timeInt-s[0] < r.window {
+			r.userDB[user][1] += 1
+			if s[1] <= r.requestLimit { // Existing user, in timeinterval and request limit
+				fmt.Printf("Call #%v, for existing user %s in time interval %v, and requestlimit %v; ratelimited: ", s[1], user, timeInt, r.requestLimit)
+				rateLimited = false
+			} else { // Existing user, in timeinterval and request limit
+				fmt.Printf("Call #%v, for existing user %s in time interval %v, but outside requestlimit %v; ratelimited: ", s[1], user, timeInt, r.requestLimit)
+				rateLimited = true
 			}
+		} else { // Existing user, quota refreshed after time interval elapsed
+			r.userDB[user] = []int64{timeInt, 1}
+			fmt.Printf("Call #1, for existing user %s outside time interval %v, in requestlimit %v; ratelimited: ", user, timeInt, r.requestLimit)
+			rateLimited = false
 		}
 	}
+	return // Named return rateLimited bool
 }
