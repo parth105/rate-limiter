b2ee122fa54ce4f41e209d8f5fe2f2078b4f33ae
parth105 <parthrshah@live.com>
2021-04-25 15:34:28 -0700

    Better commenting

--- cmd/ratelimiter/ratelimit.go
+++ cmd/ratelimiter/ratelimit.go
@@ -21,25 +21,26 @@ func NewRateLimit(w, r int64) *RateLimit {
 }
 
 func (r *RateLimit) Call(user string) (rateLimited bool) {
+	// Time in Epoch when Call is called
 	now := time.Now()
 	timeInt := now.Unix()
 
-	// Returns val, true if the user is within the time window or the number of requests
+	// Returns val, true if the user is in the userDB
 	if _, i := r.userDB[user]; !i { // New user, add to DB
 		r.userDB[user] = []int64{timeInt, 1}
 		fmt.Printf("Call #1, for new user %s in time interval %v, logged with # of requests 1; ratelimited: ", user, timeInt)
 		rateLimited = false
-	} else { // Existing user, check timeinterval
+	} else { // Existing user, check if within timeinterval
 		if s := r.userDB[user]; timeInt-s[0] < r.window {
 			r.userDB[user][1] += 1
-			if s[1] <= r.requestLimit { // Existing user, in timeinterval and request limit
+			if s[1] <= r.requestLimit { // Existing user, in timeinterval check if within request limit
 				fmt.Printf("Call #%v, for existing user %s in time interval %v, and requestlimit %v; ratelimited: ", s[1], user, timeInt, r.requestLimit)
 				rateLimited = false
-			} else { // Existing user, in timeinterval and request limit
+			} else { // Existing user, within timeinterval but outside request limit
 				fmt.Printf("Call #%v, for existing user %s in time interval %v, but outside requestlimit %v; ratelimited: ", s[1], user, timeInt, r.requestLimit)
 				rateLimited = true
 			}
-		} else { // Existing user, quota refreshed after time interval elapsed
+		} else { // Existing user, outside timeinterval (quota refreshed after time interval elapsed)
 			r.userDB[user] = []int64{timeInt, 1}
 			fmt.Printf("Call #1, for existing user %s outside time interval %v, in requestlimit %v; ratelimited: ", user, timeInt, r.requestLimit)
 			rateLimited = false
